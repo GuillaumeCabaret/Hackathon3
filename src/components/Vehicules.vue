8f091c3864d64acb4a4d609d60edb339a90aaa7c
BertrandBurel <legionxful@gmail.com>
2018-06-14 19:35:19 +0200

    connexion API cars

--- src/components/Vehicules.vue
+++ src/components/Vehicules.vue
@@ -6,33 +6,44 @@
     </div>
     <div class="refCar">
       <p>Marque:
-        <select v-model="selectedMarque">
+        <select v-model="selectedMarque" @change="callModel">
+          <option disable value="">Choississez votre marque</option>
           <option v-for="(brand, index ) of brands" :key="index">{{ brand.brandName }}</option>
         </select>
       </p>
 
       <p>Modèle:
-        <select v-model="selectedModel">
-          <option v-for="(model, index ) of models" :key="index">{{ model.brandName }}</option>
+        <select v-model="selectedModel" @change="callVersion">
+          <option disable value="">Choississez votre modèle</option>
+          <option v-for="(model, index ) of models" :key="index">{{ model.modelName }}</option>
         </select>
       </p>
 
-  <!--   <p>Couleur:
-         <select v-model="selectedCouleur">
-          <option v-for="(brand, index ) of brands" :key="index">{{ brand.brandName }}</option>
+      <p>Version:
+        <select v-model="selectedModVersion" @change="callPneu">
+          <option disable value="">Choississez la version</option>
+          <option v-for="(modVersion, index ) of modVersions" :key="index">{{ modVersion.versionName }}</option>
         </select>
       </p>
-      <p>Immatriculation:
-         <select v-model="selectedImmat">
-          <option v-for="(brand, index ) of brands" :key="index">{{ brand.brandName }}</option>
-        </select>
-      </p>
-
       <p>Types Pneus :
-         <select v-model="selectedPneus">
+        <select v-model="selectedPneus">
+          <option disable value="">Choississez vos pneus</option>
+          <option v-for="(pneu, index ) of pneus" :key="index">{{ pneu.tyreName }}</option>
+        </select>
+      </p>
+      <p>Couleur:
+        <select v-model="selectedCouleur">
+          <option disable value="">Choississez votre couleur</option>
+          <option v-for="(color, index ) of colors" :key="index">{{ color.colorName }}</option>
+        </select>
+      </p>
+      <!-- <p>Immatriculation:
+        <select v-model="selectedImmat">
           <option v-for="(brand, index ) of brands" :key="index">{{ brand.brandName }}</option>
         </select>
-      </p>  -->
+      </p>
+
+      <p> -->
     </div>
     <div class="separate">
       <hr>
@@ -43,37 +54,114 @@
 
 
 <script>
-
 export default {
   name: "Vehicules",
   data() {
-    selected: ""
+    selected: "";
     return {
       posts: [],
       errors: [],
-      brands:[],
-      models:[], 
-      brandName:[],
+      brands: [],
+      models: [],
+      colors: [],
+      pneus: [],
+      modVersions: [],
+      brandName: [],
       selectedMarque: "",
-      selectedModel:"",
-      modelValue:"",
+      selectedModel: "",
+      modelValue: "",
+      selectedCouleur: "",
+      selectedModVersion: "",
+      selectedPneus: ""
     };
   },
 
-
   created() {
-    this.axios.get(`https://fipa201gt4.execute-api.eu-central-1.amazonaws.com/dev/brand`)
+    this.axios
+      .get(
+        `https://fipa201gt4.execute-api.eu-central-1.amazonaws.com/dev/brand`
+      )
       .then(response => {
         // JSON responses are automatically parsed.
-        this.brands = response.data;}
+        this.brands = response.data;
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
+    this.axios
+      .get(
+        `https://fipa201gt4.execute-api.eu-central-1.amazonaws.com/dev/color`
       )
-
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.colors = response.data;
+      })
       .catch(e => {
         this.errors.push(e);
       });
   },
-  }
 
+  methods: {
+    callModel: function() {
+      this.axios
+        .get(
+          `https://fipa201gt4.execute-api.eu-central-1.amazonaws.com/dev/model/` +
+            this.selectedMarque
+        )
+        .then(response => {
+          this.models = response.data;
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    },
+    callVersion: function() {
+      this.axios
+        .get(
+          `https://fipa201gt4.execute-api.eu-central-1.amazonaws.com/dev/version?brand=` +
+            this.selectedMarque +
+            `&model=` +
+            this.selectedModel.substring(
+              0,
+              this.selectedModel.lastIndexOf(" ")
+            ) +
+            "  " +
+            this.selectedModel.substring(
+              this.selectedModel.lastIndexOf(" ") + 1
+            )
+        )
+        .then(response => {
+          this.modVersions = response.data;
+          console.log("modVersions");
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    },
+    callPneu: function() {
+      this.axios
+        .get(
+          `https://fipa201gt4.execute-api.eu-central-1.amazonaws.com/dev/tyre?model=` +
+            this.selectedModel.substring(
+              0,
+              this.selectedModel.lastIndexOf(" ")
+            ) +
+            "  " +
+            this.selectedModel.substring(
+              this.selectedModel.lastIndexOf(" ") + 1
+            ) +
+            `&version=` +
+            this.selectedModVersion
+        )
+        .then(response => {
+          this.pneus = response.data;
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
